a253343290ea3badb09fdd88fb896dc070af2f22
evanlin-dev <evanlin2912@gmail.com>
2024-12-05 18:02:37 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -2,14 +2,11 @@ import express from "express";
 import cors from "cors";
 import tasks from "./routes/task.js";
 
-const PORT = process.env.PORT || 5050;
 const app = express();
+const PORT = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
 app.use("/task", tasks);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+export default app;
