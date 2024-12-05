3237ccd20c343f92ce6baf10a389473c3190198a
evanlin-dev <evanlin2912@gmail.com>
2024-12-05 18:06:47 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
-import express from "express";
-import cors from "cors";
-import tasks from "./routes/task.js";
+import cors from 'cors';
+import express from 'express';
+import tasks from './routes/task.js';
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
-app.use(cors());
+const frontendUrl = "https://todo-git-main-evanlindevs-projects.vercel.app";
+
+app.use(cors({
+  origin: frontendUrl,
+  methods: "GET,POST,PATCH,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+}));
+
 app.use(express.json());
 app.use("/task", tasks);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 
 export default app;
