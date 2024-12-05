b2833d1036f2fdc621e14b1048d1f98495934b94
evanlin-dev <evanlin2912@gmail.com>
2024-12-05 18:45:58 -0500

    backend fix

--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ import cors from 'cors';
 import tasks from './routes/task.js';
 
 const app = express();
+const PORT = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
 app.use("/task", tasks);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app;
