ad209860be8369db641e1a45669e0accf0be90c5
evanlin-dev <evanlin2912@gmail.com>
2024-12-05 18:33:55 -0500

    backend fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import tasks from './routes/task.js';
-import serverless from 'serverless-http';
 
 const app = express();
 const PORT = process.env.PORT || 5050;
@@ -10,6 +9,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/task", tasks);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
